d3dc07c90e4e864a50721d3b41e14851903ddd02
Songling Liu <1843408248@qq.com>
2023-08-07 20:33:33 +0800

    尝试dataset之前先备份

--- src/app/graphs/firstGraph.js
+++ src/app/graphs/firstGraph.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import ReactEcharts from "echarts-for-react";
 
 export default function FirstGraph(){
@@ -45,17 +45,6 @@ export default function FirstGraph(){
                   data: [
                     2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3
                   ],
-                ////标记顶点。此处被用作标记极值
-                //   markPoint: {
-                //     data: [
-                //       { type: 'max', name: 'Max' },
-                //       { type: 'min', name: 'Min' }
-                //     ]
-                //   },
-                ////在图上显示一条水平线。此处被用作标记平均值
-                //   markLine: {
-                //     data: [{ type: 'average', name: 'Avg' }]
-                //   }
                 },
 
                 {
@@ -65,15 +54,6 @@ export default function FirstGraph(){
                   data: [
                     2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3
                   ],
-                //   markPoint: {
-                //     data: [
-                //       { name: 'Max', value: 182.2, xAxis: 7, yAxis: 183 },
-                //       { name: 'Min', value: 2.3, xAxis: 11, yAxis: 3 }
-                //     ]
-                //   },
-                //   markLine: {
-                //     data: [{ type: 'average', name: 'Avg' }]
-                //   }
                 }
             ]
         };
--- src/app/graphs/secondGraph.js
+++ src/app/graphs/secondGraph.js
@@ -1,8 +1,58 @@
+import React from 'react';
+import ReactEcharts from "echarts-for-react";
 
 export default function SecondGraph(){
-    return(
-    <>
-      <p>Second</p>
-    </>
-    ) ;
+  const getOption = () => {
+    return {
+      title: {
+        text: 'Stacked Line'
+      },
+      tooltip: {
+        trigger: 'axis'
+      },
+      legend: {
+        data: ['member', 'casual']
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      toolbox: {
+        show:'true',
+        feature: {
+          dataView: { show: true, readOnly: false },//数据预览
+          magicType: { show: true, type: ['line', 'bar'] },//样式：线状/条形
+          restore: { show: true },//重新加载：开
+          saveAsImage: { show: false }//保存为图像：开
+        }
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: [1, 2, 3, 4, 5, 6, 7]//这里放小时数
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          name: 'member',
+          type: 'line',
+          stack: 'Total',
+          data: [120, 132, 101, 134, 90, 230, 210]//这里放会员的数量
+        },
+        {
+          name: 'casual',
+          type: 'line',
+          stack: 'Total',
+          data: [220, 182, 191, 234, 290, 330, 310]//这里放散客的数量
+        }
+      ]
+    };
+  };
+  return <div>
+  <ReactEcharts option={getOption()} />
+</div>
 }
